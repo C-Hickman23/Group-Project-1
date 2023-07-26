9a584cbc09f0e8c96badbbf188c64c8051018801
Cardon Hickman <cardonhickman@gmail.com>
2023-07-25 21:13:35 -0600

    Set up js to grab poll info

---
+++ assets/polling.js
@@ -0,0 +1,10 @@
+$.ajax({
+    url: 'https://api.pollsapi.com/v1/get/polls-with-identifier/MUSKVZUCK',
+    crossDomain: true,
+    headers: {
+      'accept': 'application/json',
+      'api-key': 'B4VF6779FFMDRPKN7ZXWEBTAPDX3'
+    }
+  }).done(function(response) {
+    console.log(response);
+  });
