cb540b1841d12822f5e797d99586e2fa6d2e2178
patrickodea <po39dea@gmail.com>
2023-07-27 23:30:10 -0400

    TSLA Stock Updated to new API

--- assets/js/stock.js
+++ assets/js/stock.js
@@ -1,26 +1,28 @@
-const url = 'https://yh-finance-complete.p.rapidapi.com/price?symbol=TSLA';
+const url = 'https://alpha-vantage.p.rapidapi.com/query?function=TIME_SERIES_DAILY&symbol=TSLA&datatype=json';
 const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': 'cc909ce80amsh69e6d1be78828e4p14bd29jsne3188d5fc095',
-    'X-RapidAPI-Host': 'yh-finance-complete.p.rapidapi.com'
-  }
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'cc909ce80amsh69e6d1be78828e4p14bd29jsne3188d5fc095',
+		'X-RapidAPI-Host': 'alpha-vantage.p.rapidapi.com'
+	}
 };
+
 
 async function displayRegularMarketPrice() {
   try {
     const response = await fetch(url, options);
     const data = await response.json();
-    const regularMarketPrice = data.price.regularMarketPrice;
+    const openMarketPrice = data.results;
+   
 
     //Display Price
-    const responseContainer = document.getElementById('response-container');
-    responseContainer.innerHTML = `Regular Market Price: ${regularMarketPrice}`;
+    var responseContainer = document.getElementById('response-container');
+    responseContainer.innerHTML = `Regular Market Price: ${openMarketPrice}`;
   } catch (error) {
     console.error('Error fetching API:', error);
   }
 }
 
 //Button Response
-const fetchButton = document.getElementById('fetch-button');
+var fetchButton = document.getElementById('fetch-button');
 fetchButton.addEventListener('click', displayRegularMarketPrice);
